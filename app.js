bb275b29730e5451c7039fa59a7894f94f743720
David90dotcom <musicdave90@googlemail.com>
2023-08-26 23:07:17 +0200

    2

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path'); 
+const app = express();
+
+// app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
+
 
 app.set('view engine', 'ejs')
 
